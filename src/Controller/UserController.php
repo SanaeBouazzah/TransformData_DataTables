062f4a3e5348c63083b261c5cf9295c45d75310c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2025-04-14 08:40:01 +0100

    User Controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,42 +10,68 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
-#[Route('/user')]
+#[Route('/users')]
 final class UserController extends AbstractController
 {
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('gestion_users/user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_user_new', methods: ['GET'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
+        if( !empty($data['nom']) || !empty($data['prenom']) || !empty($data['username']) ||
+        !empty($data['password']) || !empty($data['position']) || !empty($data['adresse']) ||
+         !empty($data['Tel1']) || !empty($data['Tel2'])){
+            $data = json_decode($request->getContent(), true);
+            $user = new User();
+            $user->setNom($data['nom']);
+            $user->setPrenom($data['prenom']);
+            $user->setUsername($data['username']);
+            $user->setPassword($data['password']);
+            $user->setPosition($data['position']);
+            $user->setAdresse($data['adresse']);
+            $user->setTel1($data['Tel1']);
+            $user->setTel2($data['Tel2']); 
+        
             $entityManager->persist($user);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->json(['status' => 'success', 'message' => 'User created successfully!']);
         }
+        return $this->render('gestion_users/user/new.html.twig');
+    }
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+    #[Route('/', name: 'app_user_store', methods: ['GET', 'POST'])]
+    public function store(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = new User();
+        $user->setNom($data['nom']);
+        $user->setPrenom($data['prenom']);
+        $user->setUsername($data['username']);
+        $user->setPassword($data['password']);
+        $user->setPosition($data['position']);
+        $user->setAdresse($data['adresse']);
+        $user->setTel1($data['Tel1']);
+        $user->setTel2($data['Tel2'] ?? null); 
+    
+        $entityManager->persist($user);
+        $entityManager->flush();
+    
+        return $this->json(['status' => 'success', 'message' => 'User created successfully!']);
     }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
-        return $this->render('user/show.html.twig', [
+        return $this->render('gestion_users/user/show.html.twig', [
             'user' => $user,
         ]);
     }
@@ -62,7 +88,7 @@ final class UserController extends AbstractController
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('user/edit.html.twig', [
+        return $this->render('gestion_users/user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);

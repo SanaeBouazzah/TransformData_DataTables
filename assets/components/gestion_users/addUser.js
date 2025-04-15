277a0d62653f4bda8b54cfb6fb96bc76ec20f311
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2025-04-15 12:22:25 +0100

    Using AddUser to Transform data with API

---
+++ assets/components/gestion_users/addUser.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+    $('#submitUser').click(function() {
+        const userData = {
+            nom: $('#nom').val(),
+            prenom: $('#prenom').val(),
+            username: $('#username').val(),
+            password: $('#password').val(),
+            position: $('#position').val(),
+            adresse: $('#adresse').val(),
+            Tel1: $('#Tel1').val(),
+            Tel2: $('#Tel2').val(),
+            roles: ['ROLE_USER'],
+            DateCreation: new Date().toISOString(),
+        };
+    
+        // console.log('User Data:', userData); 
+        $.ajax({
+            url: Routing.generate('app_user_new'), 
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(userData),
+            success: function(response) {
+                console.log('Success:', response.message);
+                $('#userForm')[0].reset(); 
+                toastr.success('User created successfully!');
+            },
+        });
+    });
+})

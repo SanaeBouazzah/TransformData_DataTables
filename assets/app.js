99e82c8f1ad870f06f65e5742cc768aad51cd84b
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2025-04-15 12:38:27 +0100

    Add Configuration of App

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,37 @@
-import './bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap';
 import './styles/app.css';
+import './styles/style.css';
+import './images/agenda.jpg';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import Routing from './js/fos-routing/router';
+import routes from '../public/js/fos_js_routes.json';
+
+const routing = Routing.setRoutingData(routes);
+global.Routing = Routing
+
+const $ = require('jquery');
+const toastr = require('toastr');
+
+
+global.$ = global.jQuery = $;
+global.toastr = toastr;
+global.Routing = Routing;
+toastr.options = {
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: "toast-top-right",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut"
+};
+
